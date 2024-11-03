57ae609f5c3bb9338c9a13846d76d7c3417e1d17
Erick Ricardo <edck030303@gmail.com>
2024-11-03 17:27:48 -0300

    desafio

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
-console.log("Olá mundo")
+// Variáveis para armazenar o nome e a quantidade de XP do herói
+let nome = "Heroi Erick"; 
+let experiencia = 8500; 
+// Variável para armazenar o nível do herói
+let nivel;
+
+// Estrutura de decisão para classificar o nível do herói com base na experiência
+if (experiencia < 1000) {
+    nivel = "Ferro";
+} else if (experiencia >= 1001 && experiencia <= 2000) {
+    nivel = "Bronze";
+} else if (experiencia >= 2001 && experiencia <= 5000) {
+    nivel = "Prata";
+} else if (experiencia >= 5001 && experiencia <= 7000) {
+    nivel = "Ouro";
+} else if (experiencia >= 7001 && experiencia <= 8000) {
+    nivel = "Platina";
+} else if (experiencia >= 8001 && experiencia <= 9000) {
+    nivel = "Ascendente";
+} else if (experiencia >= 9001 && experiencia <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Exibindo a mensagem final
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
